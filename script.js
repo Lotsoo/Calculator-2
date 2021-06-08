0d292a9493628c372a6285e4e76df8844fedc6ab
Mikazxu <braindeathrama@gmail.com>
2021-06-09 02:56:36 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+var tombol = document.querySelector(".container-tombol");
+
+var layar = document.querySelector("#layar");
+
+var resetLayar = false;
+var bolehItung = false;
+var tmpVal = '';
+var operator = '';
+
+
+
+tombol.addEventListener("click", function (e) {
+    var tombolClick = e.target;
+    var nilaiTombol = tombolClick.innerText;
+
+    if (nilaiTombol == "C") {
+        layar.value = "";
+
+    } else if (nilaiTombol == "<") {
+        layar.value = layar.value.slice(0, -1);
+
+    } else if (nilaiTombol == "=") {
+        if (bolehItung == true) {
+            layar.value = eval(tmpVal + operator + layar.value);
+            bolehItung = false;
+        }
+    } else if (tombolClick.classList.contains('operator')) {
+        if (bolehItung == true) {
+            layar.value = eval(tmpVal + operator + layar.value);
+            bolehItung = false;
+        }
+        tmpVal = layar.value;
+        operator = nilaiTombol;
+        resetLayar = true;
+    } else {
+        if (resetLayar == true) {
+            layar.value = nilaiTombol;
+            resetLayar = false;
+            bolehItung = true;
+        } else {
+            layar.value = layar.value + nilaiTombol;
+        }
+    }
+
+
+});
